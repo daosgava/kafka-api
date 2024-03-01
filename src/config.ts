68363af9c61f489f01cda56613b2d98eee5ff060
daosgava <daosgava.garcia@gmail.com>
2024-03-01 21:06:01 +1100

    add new brokers

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const topic = "MSKTutorialTopic";
 export const brokers = [
-	"b-2-public.mskcatabumcluster.euttad.c17.kafka.us-east-1.amazonaws.com:9198",
-	"b-3-public.mskcatabumcluster.euttad.c17.kafka.us-east-1.amazonaws.com:9198",
-	"b-1-public.mskcatabumcluster.euttad.c17.kafka.us-east-1.amazonaws.com:9198",
+	"b-3-public.mskcatabumcluster.xdx43w.c17.kafka.us-east-1.amazonaws.com:9198",
+	"b-2-public.mskcatabumcluster.xdx43w.c17.kafka.us-east-1.amazonaws.com:9198",
+	"b-1-public.mskcatabumcluster.xdx43w.c17.kafka.us-east-1.amazonaws.com:9198",
 ];
 export const region = "us-east-1";
